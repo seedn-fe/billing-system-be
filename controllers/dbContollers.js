760dcb19be258f8ae2bfc7d2c4c4fe0d048c5c3c
Hank Kim <hank29206880@gmail.com>
2023-01-09 18:34:56 +0900

    정기결제 예약

--- controllers/dbContollers.js
+++ controllers/dbContollers.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 const { Contract, History } = require("../models");
 
 const createContract = (req, res) => {
@@ -10,7 +11,7 @@ const createContract = (req, res) => {
     name,
     merchant_uid,
   } = req.body;
-  console.log(req.body);
+
   Contract.create({
     merchant_uid,
     amount,
@@ -26,8 +27,79 @@ const createContract = (req, res) => {
   res.status(200).json({ link: `http://localhost:3000/${merchant_uid}` });
 };
 
-const createHistory = (req, res) => {
-  const params = req.body;
+const createHistory = async (req, res) => {
+  const {
+    customer_uid,
+    merchant_uid,
+    buyer_name,
+    buyer_email,
+    buyer_tel,
+    amount,
+  } = req.body;
+  History.create({
+    customer_uid,
+    merchant_uid,
+    amount,
+    buyer_name,
+    buyer_email,
+    buyer_tel,
+  });
+  const getToken = await axios({
+    url: "https://api.iamport.kr/users/getToken",
+    method: "post", // POST method
+    headers: { "Content-Type": "application/json" }, // "Content-Type": "application/json"
+    data: {
+      imp_key: "2734043607034140", // REST API 키e
+      imp_secret:
+        "GrPTv68uoWFd2wVQH7HM3fLIwK4zsgsYZuJK9Oc4WPxzScv8DUZVRUlwpzjgVBxAztdNGBo9xiJcQ3LW", // REST API Secret
+    },
+  });
+  const { access_token } = getToken.data.response; // 인증 토큰
+
+  // 결제(재결제) 요청
+  const paymentResult = await axios({
+    url: `https://api.iamport.kr/subscribe/payments/again`,
+    method: "post",
+    headers: { Authorization: access_token }, // 인증 토큰을 Authorization header에 추가
+    data: {
+      customer_uid,
+      merchant_uid: `mid_${new Date().getTime()}`, // 새로 생성한 결제(재결제)용 주문 번호
+      amount,
+      name: `리프 정기결제${merchant_uid}`,
+    },
+  });
+
+  const { code } = paymentResult.data;
+  if (code === 0) {
+    if (paymentResult.data.response.status === "paid") {
+      const response = await axios({
+        url: `https://api.iamport.kr/subscribe/payments/schedule`,
+        method: "post",
+        headers: { Authorization: access_token }, // 인증 토큰 Authorization header에 추가
+        data: {
+          customer_uid, // 카드(빌링키)와 1:1로 대응하는 값
+          schedules: [
+            {
+              merchant_uid: `mid_${new Date().getTime()}`, // 주문 번호
+              schedule_at: Math.floor(
+                new Date("2023-01-09T18:33:00").getTime() / 1000
+              ), // 결제 시도 시각 in Unix Time Stamp. 예: 다음 달 1일
+              amount,
+              name: `리프 정기결제_예약${merchant_uid}`,
+              buyer_name,
+              buyer_tel,
+            },
+          ],
+        },
+      });
+      console.log(response);
+      res.send({ msg: "결제성공" });
+    } else {
+      res.send({ msg: "결제실패" });
+    }
+  } else {
+    res.send({ msg: "카드사 요청 실패" });
+  }
 };
 
 const getAmount = (req, res) => {
--- models/History.js
+++ models/History.js
@@ -1,19 +1,12 @@
 module.exports = (sequelize, DataTypes) => {
   const History = sequelize.define("History", {
-    imp_uid: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
-      primaryKey: true,
-    },
     customer_uid: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
         notEmpty: true,
       },
+      primaryKey: true,
     },
     merchant_uid: {
       type: DataTypes.STRING,
