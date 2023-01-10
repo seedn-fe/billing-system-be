aa28310c34ebde75d5be7bda742c48bc6cc30144
Hank Kim <hank29206880@gmail.com>
2023-01-10 10:43:51 +0900

    config 파일 추가

---
+++ config/config.js
@@ -0,0 +1,25 @@
+const env = process.env;
+
+const development = {
+  username: env.DB_USER,
+  password: env.DB_PASSWORD,
+  database: env.DB_DATABASE,
+  host: env.DB_HOST,
+  dialect: "mysql",
+};
+const test = {
+  username: env.DB_USER,
+  password: env.DB_PASSWORD,
+  database: env.DB_DATABASE,
+  host: env.DB_HOST,
+  dialect: "mysql",
+};
+const production = {
+  username: env.DB_USER,
+  password: env.DB_PASSWORD,
+  database: env.DB_DATABASE,
+  host: env.DB_HOST,
+  dialect: "mysql",
+};
+
+module.exports = { development, test, production };
